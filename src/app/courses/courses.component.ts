b20e50c69e7bae3e95062f163237b5a82d6f47e6
Masih Niaz <masihniaz@yahoo.com>
2019-11-05 12:49:12 +0800

    Profile Forms

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from '../services/course.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -8,13 +9,24 @@ import { CourseService } from '../services/course.service';
 })
 export class CoursesComponent implements OnInit {
   courses: any[];
-  constructor(private service: CourseService) { }
+  constructor(private service: CourseService,
+              private router: Router) { }
 
   ngOnInit() {
     this.service.getAll()
     .subscribe(response => {
       this.courses = (response as any[]);
     });
+  }
+
+  // navigate to course view
+  onView(id) {
+    this.router.navigate(['/courses', id]);
+  }
+
+  // delete course
+  onDelete(id) {
+
   }
 
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -8,13 +9,22 @@ import { UserService } from '../services/user.service';
 })
 export class UsersComponent implements OnInit {
   users: any[];
-  constructor(private service: UserService) { }
+  constructor(private service: UserService,
+              private router: Router) { }
 
   ngOnInit() {
     this.service.getAll()
     .subscribe(response => {
       this.users = (response as any[]);
     });
+  }
+
+  onView(id) {
+    this.router.navigate(['users', id]);
+  }
+
+  onDelete(id) {
+
   }
 
 }
